be188b7a3c17a40a131458774bbaf1f08497c526
Kanan Garayzada <kenangerayzade2007@gmail.com>
2026-06-11 11:34:23 +0000

    Fix task 2 output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ from sys import argv
 
 if __name__ == "__main__":
     count = len(argv) - 1
-    print("{} {}".format(count, "argument." if count == 1 else "arguments:"))
+    print("{} {}".format(count, "argument:" if count == 1 else "arguments:"))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
