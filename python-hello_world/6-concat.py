01a970be72bb97fc4d58e7cd745c576e156dd50b
Kanan Garayzada <kenangerayzade2007@gmail.com>
2026-06-20 08:28:59 +0000

    Fix concat task

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome"
-str2 = "Holberton School"
-str3 = f"{str1} to {str2}!"
-print(str3)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1+" "+str2
+print(f"Welcome to {str1}!")
